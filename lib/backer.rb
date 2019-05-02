be1281b9228727a7e1dbb8f033981f1908706bcf
Anthony Rurup <tcrurup@gmail.com>
2019-05-02 19:48:42 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,13 +1,13 @@
 class Backer
   
-  attr_accessor :backed_projects
+  attr_reader :backed_projects
   
   def initialize
     @backed_projects = []  
   end
   
   def back_project(project)
-    self.backed_projects << project
+    @backed_projects << project
   end
   
   
